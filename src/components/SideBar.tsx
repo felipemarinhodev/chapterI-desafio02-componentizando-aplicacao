7621ff4ced80ee7a2ba67cf2e87052c321e9bab7
Felipe Marinho <felipmarinho@gmail.com>
2021-07-22 12:43:16 -0300

    improve components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => React.ReactNode;
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  handleClickButton,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
